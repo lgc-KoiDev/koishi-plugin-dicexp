fd69de3979c7e25c1a1365710f33256cb35a9f10
student_2333 <lgc2333@126.com>
2024-04-22 07:43:38 +0800

    complete

---
+++ readme.md
@@ -0,0 +1,57 @@
+<!-- markdownlint-disable MD026 MD031 MD033 MD036 MD041 -->
+
+<div align="center">
+
+<a href="https://koishi.chat/zh-CN/market/">
+  <img src="https://raw.githubusercontent.com/lgc2333/koishi-workspace/master/readme/koishi-plugin.png" width="180" height="180" alt="NoneBotPluginLogo">
+</a>
+
+<p>
+  <img src="https://raw.githubusercontent.com/lgc2333/koishi-workspace/master/readme/KoishiPlugin.svg" width="240" alt="NoneBotPluginText">
+</p>
+
+# Koishi-Plugin-Dicexp
+
+_✨ 用 Bot 指令执行 Dicexp 表达式 ✨_
+
+<a href="./LICENSE">
+  <img src="https://img.shields.io/github/license/lgc2333/koishi-plugin-dicexp.svg" alt="license">
+</a>
+<a href="https://www.npmjs.com/package/koishi-plugin-dicexp">
+  <img src="https://img.shields.io/npm/v/koishi-plugin-dicexp" alt="pypi">
+</a>
+<a href="https://www.npmjs.com/package/koishi-plugin-dicexp">
+  <img src="https://img.shields.io/npm/dm/koishi-plugin-dicexp" alt="pypi download">
+</a>
+<a href="https://wakatime.com/badge/user/b61b0f9a-f40b-4c82-bc51-0a75c67bfccf/project/79af41ae-0173-4c1f-9db2-f87d94569c76">
+  <img src="https://wakatime.com/badge/user/b61b0f9a-f40b-4c82-bc51-0a75c67bfccf/project/79af41ae-0173-4c1f-9db2-f87d94569c76.svg" alt="wakatime">
+</a>
+
+</div>
+
+## 📖 介绍
+
+没有
+
+## 💡 鸣谢
+
+### [umajho/dicexp](https://github.com/umajho/dicexp)
+
+- 本插件的上游项目
+
+## 📞 联系
+
+QQ：3076823485  
+Telegram：[@lgc2333](https://t.me/lgc2333)  
+吹水群：[1105946125](https://jq.qq.com/?_wv=1027&k=Z3n1MpEp)  
+邮箱：<lgc2333@126.com>
+
+## 💰 赞助
+
+**[赞助我](https://blog.lgc2333.top/donate)**
+
+感谢大家的赞助！你们的赞助将是我继续创作的动力！
+
+## 📝 更新日志
+
+还没有
---
+++ src/index.ts
@@ -0,0 +1,63 @@
+import { Context, Schema } from 'koishi';
+
+export const name = 'dicexp';
+
+export interface Config {}
+
+export const Config: Schema<Config> = Schema.object({});
+
+export async function applyReal(ctx: Context) {
+  const { Evaluator, asScope } = await import('@dicexp/naive-evaluator');
+  const { functionScope, operatorScope } = await import(
+    '@dicexp/naive-evaluator-builtins'
+  );
+
+  const topLevelScope = asScope([operatorScope, functionScope]);
+  const evaluator = new Evaluator({
+    topLevelScope,
+    randomSourceMaker: 'xorshift7',
+  });
+
+  ctx.i18n.define('zh-CN', require('./locales/zh-CN.yml'));
+  ctx.i18n.define('zh', require('./locales/zh-CN.yml'));
+  ctx.i18n.define('en-US', require('./locales/en-US.yml'));
+  ctx.i18n.define('en', require('./locales/en-US.yml'));
+
+  ctx
+    .command('dicexp <exp:text>')
+    .option('seed', '-s [seed:number]')
+    .option('verbose', '-v [verbose:boolean]')
+    .action(async ({ session, options }, exp) => {
+      if (!exp) return session.execute('help dicexp');
+
+      const seed =
+        options.seed || crypto.getRandomValues(new Uint32Array(1))[0]!;
+      const result = evaluator.evaluate(exp, {
+        execution: { seed },
+      });
+
+      console.log(options.verbose);
+      if (result[0] === 'ok') {
+        const res = result[1];
+        return options.verbose
+          ? session.text('dicexp.reply.result-verbose', [seed, res])
+          : session.text('dicexp.reply.result', [res]);
+      }
+
+      const [, type, err] = result;
+      const errMsg = err.message;
+      return session.text('dicexp.reply.error', [
+        session.text(`dicexp.error.${type}`),
+        errMsg,
+      ]);
+    });
+}
+
+export async function apply(ctx: Context) {
+  try {
+    await applyReal(ctx);
+  } catch (e) {
+    ctx.scope.dispose();
+    throw e;
+  }
+}
