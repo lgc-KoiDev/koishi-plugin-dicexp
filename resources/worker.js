a4ca25e8bfcf036f7891c55da3ac72a9c35f3724
student_2333 <lgc2333@126.com>
2024-04-24 11:54:24 +0800

    up

---
+++ resources/worker.js
@@ -0,0 +1,13 @@
+(async () => {
+  const { Evaluator, asScope } = await import('@dicexp/naive-evaluator');
+  const { functionScope, operatorScope } = await import(
+    '@dicexp/naive-evaluator-builtins'
+  );
+  const { startWorkerServer } = await import(
+    '@dicexp/naive-evaluator-in-worker'
+  );
+
+  const topLevelScope = asScope([operatorScope, functionScope]);
+
+  startWorkerServer((opts) => new Evaluator(opts), topLevelScope);
+})();
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,62 @@
+import path from 'node:path';
+import { pathToFileURL } from 'node:url';
+
+import type { EvaluationResult } from '@dicexp/interface';
 import { Context, Schema } from 'koishi';
+import Worker from 'web-worker';
+
+import I18nEnUS from './locales/en-US.yml';
+import I18nZhCN from './locales/zh-CN.yml';
+
+// if (!globalThis.Worker) globalThis.Worker = Worker;
 
 export const name = 'dicexp';
 
-export interface Config {}
+export interface Config {
+  heartbeatTimeout: number;
+}
 
-export const Config: Schema<Config> = Schema.object({});
+export const Config: Schema<Config> = Schema.intersect([
+  Schema.object({
+    heartbeatTimeout: Schema.number().default(1000),
+  }),
+]).i18n({
+  'zh-CN': I18nZhCN._config,
+  zh: I18nZhCN._config,
+  'en-US': I18nEnUS._config,
+  en: I18nEnUS._config,
+});
 
-export async function applyReal(ctx: Context) {
-  const { Evaluator, asScope } = await import('@dicexp/naive-evaluator');
-  const { functionScope, operatorScope } = await import(
-    '@dicexp/naive-evaluator-builtins'
+export async function applyReal(ctx: Context, config: Config) {
+  const { EvaluatingWorkerManager } = await import(
+    '@dicexp/naive-evaluator-in-worker'
   );
 
-  const topLevelScope = asScope([operatorScope, functionScope]);
-  const evaluator = new Evaluator({
-    topLevelScope,
-    randomSourceMaker: 'xorshift7',
-  });
+  const workerJsUrl = new URL(
+    pathToFileURL(path.join(__dirname, '../resources/worker.js')),
+  );
+  const workerProvider = () => new Worker(workerJsUrl);
+  const evaluate = (exp: string, seed: number) =>
+    new Promise<EvaluationResult>((resolve) => {
+      const manager = new EvaluatingWorkerManager(
+        workerProvider,
+        async (ready) => {
+          if (!ready) return;
+          const result = await manager.evaluateRemote(exp, {
+            execution: { seed },
+          });
+          manager.destroy();
+          resolve(result);
+        },
+        { newEvaluatorOptions: { randomSourceMaker: 'xorshift7' } },
+        { heartbeatTimeout: { ms: config.heartbeatTimeout } },
+      );
+    });
 
-  ctx.i18n.define('zh-CN', require('./locales/zh-CN.yml'));
-  ctx.i18n.define('zh', require('./locales/zh-CN.yml'));
-  ctx.i18n.define('en-US', require('./locales/en-US.yml'));
-  ctx.i18n.define('en', require('./locales/en-US.yml'));
+  ctx.i18n.define('zh-CN', I18nZhCN);
+  ctx.i18n.define('zh', I18nZhCN);
+  ctx.i18n.define('en-US', I18nEnUS);
+  ctx.i18n.define('en', I18nEnUS);
 
   ctx
     .command('dicexp <exp:text>')
@@ -32,11 +67,8 @@ export async function applyReal(ctx: Context) {
 
       const seed =
         options.seed || crypto.getRandomValues(new Uint32Array(1))[0]!;
-      const result = evaluator.evaluate(exp, {
-        execution: { seed },
-      });
+      const result = await evaluate(exp, seed);
 
-      console.log(options.verbose);
       if (result[0] === 'ok') {
         const res = JSON.stringify(result[1]);
         return options.verbose
@@ -53,9 +85,9 @@ export async function applyReal(ctx: Context) {
     });
 }
 
-export async function apply(ctx: Context) {
+export async function apply(ctx: Context, config: Config) {
   try {
-    await applyReal(ctx);
+    await applyReal(ctx, config);
   } catch (e) {
     setTimeout(() => ctx.scope.dispose());
     throw e;
